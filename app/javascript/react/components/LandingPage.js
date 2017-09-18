9434f2d6c609ceba5b5e03373b7687d9e2a347d3
Eben <eben.lowe@gmail.com>
2017-09-18 11:32:33 -0400

    Change landing page from Rails to React.

---
+++ app/javascript/react/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LandingPage = props => {
+  return(
+    <div className='row'>
+      <div className='small-12 columns'>
+        <div className='text-center top-spacer'>
+          <h1>Port Katherine</h1>
+          <p>Coming Soon</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
