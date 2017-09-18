4756d88db5abb536b5ae097e7f2ddb79d23ebe4d
Eben <eben.lowe@gmail.com>
2017-09-18 14:20:18 -0400

    Add redux, router, redux-form, etc. and set up feature flag.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,51 @@
+import 'whatwg-fetch'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createBrowserHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
+import thunkMiddleware from 'redux-thunk'
+
 import LandingPage from '../react/components/LandingPage'
+
+const history = createBrowserHistory()
+
+const middlewares = [thunkMiddleware, routerMiddleware(history)]
+
+const store = createStore(
+  combineReducers({
+    form: formReducer,
+    router: routerReducer
+  }),
+  applyMiddleware(...middlewares)
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   let reactElement = document.getElementById('react')
 
   if (reactElement) {
-    ReactDOM.render(
-      <LandingPage />,
-      reactElement
-    )
+    if (process.env.RELEASE_THE_KRAKEN === 'true') {
+      ReactDOM.render(
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <div>
+              <Route exact path='/' component={LandingPage} />
+            </div>
+          </ConnectedRouter>
+        </Provider>,
+        reactElement
+      )
+    } else {
+      ReactDOM.render(
+        <LandingPage />,
+        reactElement
+      )
+    }
   }
 })
