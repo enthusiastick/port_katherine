beab1f0fc3e3bdbc785d6b0fcead7f52842b9a64
Eben <eben.lowe@gmail.com>
2017-10-16 17:10:29 -0400

    Fix up serializer weirdness for admin event bookings.

--- app/serializers/admin/event_serializer.rb
+++ app/serializers/admin/event_serializer.rb
@@ -1,6 +1,12 @@
 class Admin::EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :slug, :start_time, :end_time, :dates, :address, :description, :latitude, :longitude
-  has_many :bookings
+  attributes :id, :name, :slug, :start_time, :end_time, :dates, :address, :description, :latitude, :longitude, :bookings
+
+  def bookings
+    ActiveModel::SerializableResource.new(
+      object.bookings,
+      each_serializer: Admin::BookingSerializer
+    ).serializable_hash[:bookings]
+  end
 
   def end_time
     object.end_time.strftime("%Y-%m-%dT%H:%M")
