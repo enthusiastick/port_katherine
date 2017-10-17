4241e29eec2e3529587af963426063d91c5f69c2
Eben <eben.lowe@gmail.com>
2017-10-16 22:28:37 -0400

    Switch admin users table over to sortable, filterable reactable.

--- app/javascript/react/subApps/admin/userManager/containers/UsersIndexContainer.js
+++ app/javascript/react/subApps/admin/userManager/containers/UsersIndexContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import Table from '../components/Table'
+import { Table, Td, Th, Thead, Tr } from 'reactable'
 
 import restrictAccess from '../../constants/restrictAccess'
 
@@ -18,11 +18,38 @@ class UsersIndexContainer extends Component {
   }
 
   render() {
+    let tableRows = this.props.adminUsers.map(user => {
+      let name = `${user.firstName} ${user.lastName}`
+
+      return(
+        <Tr key={user.id}>
+          <Td column='handle' data={user.handle} />
+          <Td column='name' data={name} />
+          <Td column='email' value={user.email}>
+            <a href={`mailto:${user.email}`}>{user.email}</a>
+          </Td>
+        </Tr>
+      )
+    })
+
     return(
       <div className='row'>
         <div className='small-12 columns'>
           <h1 className='text-center top-padded'>Users</h1>
-          <Table users={this.props.adminUsers} />
+          <Table
+            className='hover'
+            data={[]}
+            filterable={['handle', 'email', 'name']}
+            itemsPerPage={25}
+            sortable={true}
+          >
+            <Thead>
+              <Th column='handle'>Handle</Th>
+              <Th column='name'>Name</Th>
+              <Th column='email'>Email</Th>
+            </Thead>
+            {tableRows}
+          </Table>
         </div>
       </div>
     )
