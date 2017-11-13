4c83bb5781a4e22a2b43a2e94808a803c88e4732
Eben <eben.lowe@gmail.com>
2017-11-13 10:51:17 -0500

    Add some Google Docs to the links static page.

--- app/javascript/react/sharedResources/components/Resources.js
+++ app/javascript/react/sharedResources/components/Resources.js
@@ -21,6 +21,17 @@ const Resources = props => {
               <i className='fa fa-pinterest-square' />&nbsp;More Pinterest Inspiration
             </h3>
           </a>
+          <h1 className='text-center'><i className='fa fa-google' />oogle Docs</h1>
+          <a href='https://docs.google.com/document/d/1OagOMEWg6QZ2ZwCPnY9meqEs980EPius3wGvytP53_U' target='_blank'>
+            <h3>
+              <i className='fa fa-file-text' /> Costuming Guide
+            </h3>
+          </a>
+          <a href='https://docs.google.com/document/d/1iYLYDeuuOI8OUN0kpzKm9N0Vdfi6oahHaWAYxCp0LvQ' target='_blank'>
+            <h3>
+              <i className='fa fa-file-text' /> Advanced Header Paths
+            </h3>
+          </a>
         </div>
       </div>
     </div>
