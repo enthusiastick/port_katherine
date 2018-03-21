394186114671585f11b1f38d19b6d28bd9458b5b
Eben <eben.lowe@gmail.com>
2018-03-21 19:22:33 -0400

    Hotfix character spent_cycle default.

---
+++ db/migrate/20180321231216_fix_default_character_spent_cycle.rb
@@ -0,0 +1,19 @@
+class FixDefaultCharacterSpentCycle < ActiveRecord::Migration[5.1]
+  def up
+    change_column :characters, :spent_cycle, :integer, default: -30, null: false
+    Character.all.each do |character|
+      adjusted_spent_cycle = character.spent_cycle + 3
+      character.update(spent_cycle: adjusted_spent_cycle)
+      Tally.create(
+        annotation: character.tally_annotation,
+        character: character,
+        description: "audited spent cycle total.",
+        user: User.admin.first
+      )
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321195210) do
+ActiveRecord::Schema.define(version: 20180321231216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20180321195210) do
     t.string "name", null: false
     t.string "non_sequential_id", null: false
     t.integer "spent", default: 0, null: false
-    t.integer "spent_cycle", default: -33, null: false
+    t.integer "spent_cycle", default: -30, null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
