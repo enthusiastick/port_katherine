ea50c6c3b7de3e99e3854973c577245b1c7cb3da
Eben <eben.lowe@gmail.com>
2018-03-25 11:25:34 -0400

    Change skill summary output to check for checkbox skills

--- app/javascript/react/subApps/characterBuilder/components/header/Summary.js
+++ app/javascript/react/subApps/characterBuilder/components/header/Summary.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
+const Ranks = props => {
+  if (props.maxRank === 1 && props.ranks === 1) {
+    return <i className='fa fa-check' />
+  }
+
+  if (props.maxRank === 1 && props.ranks != 1) {
+    return <i className='fa fa-times' />
+  }
+
+  return  <span>{props.ranks}</span>
+}
+
 const Summary = props => {
   const skillList = props.skills.map(skill => {
     if (skill.characterSkillId) {
       return(
-        <li key={skill.characterSkillId}>{skill.name} {skill.ranks}</li>
+        <li key={skill.characterSkillId}>{skill.name} <Ranks {...skill} /></li>
       )
     }
   })
+
   return(
     <div className='callout'>
       <p><strong>{props.name}</strong></p>
