664c48b03729b37cfae71deb16f86359a16953e3
Eben Lowe <eben.lowe@gmail.com>
2018-03-30 15:44:27 -0400

    Update rule and supplement links on the downloads page.

--- app/javascript/react/sharedResources/components/Downloads.js
+++ app/javascript/react/sharedResources/components/Downloads.js
@@ -6,13 +6,13 @@ const Downloads = props => {
       <div className='row'>
         <div className='small-11 medium-9 large-7 small-centered columns'>
           <h1 className='text-center top-padded'>Rulebooks</h1>
-          <a href='https://s3-us-west-2.amazonaws.com/portkatherine/rules/Port_Katherine_Rules_v1.0.pdf' target='_blank'>
+          <a href='https://s3-us-west-2.amazonaws.com/portkatherine/rules/Port_Katherine_Rules_v1.1.pdf' target='_blank'>
             <h3 className='text-center'>
-              <i className='fa fa-book' />&nbsp;Rulebook (version 1.0)
+              <i className='fa fa-book' />&nbsp;Rulebook (version 1.1)
             </h3>
           </a>
           <h1 className='text-center'>Supplements</h1>
-          <a href='https://s3.us-west-2.amazonaws.com/portkatherine/supplements/Port_Katherine_approved_blaster_types.pdf' target='_blank'>
+          <a href='https://s3-us-west-2.amazonaws.com/portkatherine/supplements/Port_Katherine_approved_blaster_types_v1.1.pdf' target='_blank'>
             <h3 className='text-center'>
               <i className='fa fa-file-text' /> Approved Blaster Types
             </h3>
