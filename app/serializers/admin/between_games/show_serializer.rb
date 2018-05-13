670b2825b0deeabc64898592081394ca682c4993
Eben <eben.lowe@gmail.com>
2018-05-12 23:40:14 -0400

    Resolve comment display bug by making admin comment serializer explicit

--- app/serializers/admin/between_games/show_serializer.rb
+++ app/serializers/admin/between_games/show_serializer.rb
@@ -2,6 +2,9 @@ class Admin::BetweenGames::ShowSerializer < Admin::BetweenGames::IndexSerializer
   attributes :body
 
   has_many :comments do
-    object.comments.order(:created_at)
+    ActiveModelSerializers::SerializableResource.new(
+      object.comments.order(:created_at),
+      each_serializer: ::Admin::CommentSerializer,
+    ).serializable_hash[:comments]
   end
 end
