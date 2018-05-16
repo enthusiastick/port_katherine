9faab788d4ba9401685ff637fad400e0ef9bf831
Eben Lowe <eben.lowe@gmail.com>
2018-05-16 10:40:05 -0400

    Adding default character name as user title to Discourse SSO payload

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -8,6 +8,7 @@ class DiscourseSsoController < ApplicationController
     sso.name = current_user.full_name
     sso.username = current_user.handle
     sso.external_id = current_user.id
+    sso.title = current_user.default_character.name if current_user.default_character.present?
     sso.sso_secret = secret
 
     redirect_to sso.to_url("#{ENV['FORUMS_URL']}/session/sso_login")
--- app/models/single_sign_on.rb
+++ app/models/single_sign_on.rb
@@ -1,6 +1,6 @@
 class SingleSignOn
   ACCESSORS = [:nonce, :name, :username, :email, :avatar_url, :avatar_force_update, :require_activation,
-               :about_me, :external_id, :return_sso_url, :admin, :moderator, :suppress_welcome_message]
+               :about_me, :external_id, :return_sso_url, :admin, :moderator, :suppress_welcome_message, :title]
   FIXNUMS = []
   BOOLS = [:avatar_force_update, :admin, :moderator, :require_activation, :suppress_welcome_message]
   NONCE_EXPIRY_TIME = 10.minutes
