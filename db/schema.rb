c3b45edbeffe5cac1a61108864d529efb262f803
Eben Lowe <eben.lowe@gmail.com>
2019-02-05 16:32:15 -0500

    Add attribute to character for tracking CP gifts received per cycle.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_160722) do
+ActiveRecord::Schema.define(version: 2019_02_05_192428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_01_24_160722) do
     t.datetime "updated_at", null: false
     t.datetime "archived_at"
     t.integer "archived_by_id"
+    t.decimal "gifts_received_cycle", precision: 4, scale: 2, default: "0.0"
     t.index ["non_sequential_id"], name: "index_characters_on_non_sequential_id", unique: true
   end
 
